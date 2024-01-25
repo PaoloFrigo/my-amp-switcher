6b4e2a9d1ab5875e30b391706d2dd951124fd9dd
Paolo Frigo <paolofrigo@gmail.com>
2024-01-25 02:29:12 +0100

    feat: add font and size

--- MyAmpSwitcher.py
+++ MyAmpSwitcher.py
@@ -3,7 +3,7 @@ import mido
 import os
 from PyQt5.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget, QComboBox, QLabel, \
     QHBoxLayout, QFileDialog
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QFont
 
 # Global variables
 script_directory = os.path.dirname(os.path.realpath(__file__))
@@ -23,25 +23,10 @@ def load_profile_data(profile_name):
         return json.load(json_file)
 
 def create_window(profile_data):
-    global window
+    global window, midi_channel_combobox
     window = QWidget()
     window.setWindowTitle(profile_data['name'])
     layout = QVBoxLayout(window)
-
-    sorted_buttons = sorted(profile_data.get('buttons', []), key=lambda x: x.get('order', 0))
-
-    # Create a horizontal layout for channel buttons
-    channel_buttons_layout = QHBoxLayout()
-
-    for button_info in sorted_buttons:
-        pc_number = button_info.get('program_change', 0)
-        name = button_info.get('name', 'Unknown')
-        button = QPushButton(name)
-        button.setMinimumHeight(40)
-        button.clicked.connect(lambda _, ch=pc_number: send_program_change(ch))
-        channel_buttons_layout.addWidget(button)
-
-    layout.addLayout(channel_buttons_layout)
 
     # MIDI Output ComboBox
     midi_output_label = QLabel("MIDI Output:")
@@ -55,7 +40,7 @@ def create_window(profile_data):
     midi_channel_label = QLabel("Channel:")
     midi_channel_combobox = QComboBox()
     midi_channel_combobox.addItems(map(str, range(128)))  # Adding values 0 to 127
-    midi_channel_combobox.setCurrentText(str(profile_data["channel"]))
+    midi_channel_combobox.setCurrentText(str(profile_data.get("channel", settings["channel"])))
     midi_channel_combobox.currentIndexChanged.connect(select_midi_channel)
 
     # Save Button
@@ -68,14 +53,30 @@ def create_window(profile_data):
 
     # Create a horizontal layout for MIDI output, channel, save, and profile change buttons
     midi_layout = QHBoxLayout()
+    midi_layout.addWidget(profile_change_button)
     midi_layout.addWidget(midi_output_label)
     midi_layout.addWidget(midi_output_combobox)
     midi_layout.addWidget(midi_channel_label)
     midi_layout.addWidget(midi_channel_combobox)
     midi_layout.addWidget(save_button)
-    midi_layout.addWidget(profile_change_button)
 
     layout.addLayout(midi_layout)
+
+    sorted_buttons = sorted(profile_data.get('buttons', []), key=lambda x: x.get('order', 0))
+
+    # Create a horizontal layout for channel buttons
+    channel_buttons_layout = QHBoxLayout()
+
+    for button_info in sorted_buttons:
+        pc_number = button_info.get('program_change', 0)
+        name = button_info.get('name', 'Unknown')
+        button = QPushButton(name)
+        button.setMinimumHeight(40)
+        button.setFont(QFont(settings["font"],settings["size"]))
+        button.clicked.connect(lambda _, ch=pc_number: send_program_change(ch))
+        channel_buttons_layout.addWidget(button)
+
+    layout.addLayout(channel_buttons_layout)
 
     title_width = len(profile_data['name']) * 10 + 50
     window.resize(title_width, 100)
@@ -86,6 +87,10 @@ def create_window(profile_data):
     app.setWindowIcon(app_icon)
 
     return window
+
+   
+
+
 
 # SETTINGS
 settings = load_settings()
