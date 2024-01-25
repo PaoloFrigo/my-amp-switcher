2be8ab4bc1b67e0266dbe80e80daeb2e8ccd4145
Paolo Frigo <paolofrigo@gmail.com>
2024-01-25 10:35:36 +0100

    feat: update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
 
 python3 setup.py py2app
-cp dist/MyAmpSwitcher.app .
+hdiutil create -format UDZO -srcfolder dist/MyAmpSwitcher.app -o MyAmpSwitcher.dmg --overwrite
+hdiutil attach MyAmpSwitcher.dmg
+cp icon.jpeg /Volumes/MyAmpSwitcher/.background
