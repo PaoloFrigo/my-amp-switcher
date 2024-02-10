a8b3cf706625961ca4f9c4bdf84753c10ceb8dde
Paolo Frigo <paolofrigo@gmail.com>
2024-02-11 00:48:50 +0100

    fix: simplify build

--- build.sh
+++ build.sh
@@ -1,7 +1,32 @@
-#!/bin/zsh
+rm -rf build dist
 
-python3 setup.py py2app
-rm releases/MyAmpSwitcher.dmg
-hdiutil create -format UDZO -srcfolder dist/MyAmpSwitcher.app -o releases/MyAmpSwitcher.dmg 
-hdiutil attach MyAmpSwitcher.dmg
-cp icon.jpeg /Volumes/MyAmpSwitcher/.background
+pyinstaller --name 'MyAmpSwitcher' \
+            --icon 'icon.icns' \
+            --noconsole  \
+            --windowed  \
+            --add-data './settings.json:.' \
+            --add-data './icon.icns:.' \
+            --add-data './profiles/*:profiles' \
+            --add-data='./.venv/lib/python3.11/site-packages/PyQt5/Qt5/plugins/platforms:./PyQt5/Qt/plugins/platforms'\
+            --additional-hooks-dir='./' \
+            --hidden-import mido.backends.rtmidi \
+            --hidden-import pyqt5 \
+            MyAmpSwitcher.py
+
+
+
+mkdir -p dist/dmg
+cp -r "dist/MyAmpSwitcher.app" dist/dmg
+
+create-dmg \
+  --volname "MyAmpSwitcher" \
+  --volicon "icon.ico" \
+  --window-pos 200 120 \
+  --window-size 600 300 \
+  --icon-size 100 \
+  --icon "MyAmpSwitcher.app" 175 120 \
+  --hide-extension "MyAmpSwitcher.app" \
+  --app-drop-link 425 120 \
+  "dist/MyAmpSwitcher.dmg" \
+  "dist/dmg/"
+
--- setup.py
+++ setup.py
@@ -1,48 +1,33 @@
-from setuptools import setup, find_packages
+import glob
+from setuptools import setup
 
-# Version and GitHub link
-VERSION = "1.0.0"
-GITHUB_URL = "https://github.com/paolofrigo/my-amp-switcher"
+required_libraries = ["mido==1.2.9", "PyQt5==5.15.2", "python-rtmidi"]
 
 APP = ["MyAmpSwitcher.py"]
 DATA_FILES = [
-    (".", ["profiles"]),  # Include the 'profiles' folder and its contents
     (
-        ".",
-        ["icon.icns", "icon.ico", "settings.json"],
-    ),  # Include 'icon.icns', 'icon.ico', and 'settings.json' in the root directory
+        "profiles",
+        glob.glob("profiles/**/*", recursive=True),
+    ),
+    ("", ["settings.json"]),
 ]
-
-# macOS options for py2app
-MAC_OPTIONS = {
+OPTIONS = {
     "argv_emulation": True,
-    "packages": ["mido", "PyQt5"],
-    "includes": ["json"],
-    "iconfile": "icon.icns",
-    "plist": {"CFBundleVersion": VERSION},  # Set the CFBundleVersion
-}
-
-# Windows options for pyinstaller
-WINDOWS_OPTIONS = {
-    'packages': ['mido', 'PyQt5'],
-    'includes': ['json'],
-    'icon': 'icon.ico',
+    "resources": [
+        "icon.icns",
+        "icon.ico",
+        "settings.json",
+    ] + glob.glob("profiles/**/*", recursive=True),
+    "includes": required_libraries,
+    "packages": ["PyQt5", "mido"],
+    "plist": {
+        "CFBundleIconFile": "icon.icns",
+    },
 }
 
 setup(
     app=APP,
     data_files=DATA_FILES,
-    options={
-        'py2app': MAC_OPTIONS,
-        'pyinstaller': WINDOWS_OPTIONS,
-    },
-    install_requires=[
-        'mido',
-        'PyQt5',
-        'python-rtmidi',
-    ],
-    version=VERSION,
-    url=GITHUB_URL,
-    packages=find_packages(),
-
+    options={"py2app": OPTIONS},
+    setup_requires=["py2app"],
 )
