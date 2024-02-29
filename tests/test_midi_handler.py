4c4b9a09dd1f8ca92e6052d2ecd699b398d2d230
Paolo Frigo <paolofrigo@gmail.com>
2024-02-29 11:46:45 +0100

    feat: add unit tests

---
+++ tests/test_midi_handler.py
@@ -0,0 +1,172 @@
+import mido
+import json
+from unittest.mock import MagicMock, patch, call
+from midi.midi_handler import MidiHandler
+
+
+def test_init():
+    window_mock = MagicMock()
+    midi_handler = MidiHandler(window_mock)
+    assert midi_handler.window == window_mock
+    assert midi_handler.midi_input is None
+    assert midi_handler.midi_message_list == []
+    assert midi_handler.midi_output is None
+    assert midi_handler.midi_channel is None
+
+
+def test_set_midi_channel():
+    window_mock = MagicMock()
+    midi_handler = MidiHandler(window_mock)
+    midi_handler.set_midi_channel(1)
+    assert midi_handler.midi_channel == 1
+
+
+# def test_set_midi_output():
+#     window_mock = MagicMock()
+#     midi_handler = MidiHandler(window_mock)
+
+#     output_port_mock = MagicMock()
+#     with patch.object(midi_handler, 'open_output', return_value=output_port_mock):
+#         midi_handler.set_midi_output("output_port")
+
+#     assert midi_handler.midi_output == output_port_mock
+#     midi_handler.open_output.assert_called_once_with("output_port")
+
+
+def test_start_midi_input():
+    window_mock = MagicMock()
+    midi_handler = MidiHandler(window_mock)
+
+    with patch.object(mido, "open_input") as mock_open_input:
+        midi_handler.start_midi_input()
+
+    assert midi_handler.midi_input == mock_open_input.return_value
+    assert window_mock.update_status_bar.call_args_list == [
+        call("MIDI Input Capture\n")
+    ]
+    assert midi_handler.midi_input.callback == midi_handler.handle_midi_message
+
+
+def test_start_midi_input_exception():
+    window_mock = MagicMock()
+    midi_handler = MidiHandler(window_mock)
+
+    with patch.object(mido, "open_input", side_effect=Exception("Test Error")):
+        midi_handler.start_midi_input()
+
+    assert midi_handler.midi_input is None
+    assert window_mock.update_status_bar.call_args_list == [
+        call("Error starting MIDI input: Test Error\n")
+    ]
+
+
+# def test_stop_midi_input():
+#     window_mock = MagicMock()
+#     midi_handler = MidiHandler(window_mock)
+#     midi_handler.midi_input = MagicMock()
+
+#     midi_handler.stop_midi_input()
+
+#     assert midi_handler.midi_input.close.called
+#     assert window_mock.update_status_bar.call_args_list == [call("MIDI Input Stopped\n")]
+#     assert midi_handler.midi_input is None
+
+
+def test_handle_midi_message():
+    window_mock = MagicMock()
+    midi_handler = MidiHandler(window_mock)
+
+    midi_message_mock = MagicMock()
+    midi_handler.handle_midi_message(midi_message_mock)
+
+    assert midi_handler.midi_message_list == [midi_message_mock]
+    assert window_mock.midi_log.append.called_with(f"{midi_message_mock}")
+    assert window_mock.update_status_bar.called_with(
+        f"Received MIDI message: {midi_message_mock}"
+    )
+
+
+def test_generate_profile():
+    window_mock = MagicMock()
+    midi_handler = MidiHandler(window_mock)
+
+    midi_handler.midi_message_list = [
+        mido.Message("program_change", channel=1, program=1),
+        mido.Message("control_change", channel=1, control=2, value=127),
+    ]
+
+    midi_handler.generate_profile()
+
+    assert window_mock.update_status_bar.called_with("Profile generated")
+    assert window_mock.right_column.clear.called
+    assert window_mock.right_column.append.called_with(
+        json.dumps(
+            {
+                "name": "profilename",
+                "channel": 0,
+                "buttons": [
+                    {"order": 0, "name": "button 1", "program_change": 1},
+                    {"order": 1, "name": "button 2", "cc_number": 2, "cc_value": 127},
+                ],
+            },
+            indent=2,
+        )
+    )
+
+
+def test_send_midi_message_program_change():
+    window_mock = MagicMock()
+    midi_handler = MidiHandler(window_mock)
+    midi_handler.midi_output = MagicMock()
+    midi_handler.midi_channel = 1
+
+    midi_handler.send_midi_message(pc_number=1, cc_number=None, cc_value=None)
+
+    assert midi_handler.midi_output.send.called_with(
+        mido.Message("program_change", channel=1, program=1)
+    )
+    assert window_mock.update_status_bar.called_with("Midi Program: 1")
+
+
+def test_send_midi_message_control_change():
+    window_mock = MagicMock()
+    midi_handler = MidiHandler(window_mock)
+    midi_handler.midi_output = MagicMock()
+    midi_handler.midi_channel = 1
+
+    midi_handler.send_midi_message(pc_number=None, cc_number=2, cc_value=127)
+
+    assert midi_handler.midi_output.send.called_with(
+        mido.Message("control_change", channel=1, control=2, value=127)
+    )
+    assert window_mock.update_status_bar.called_with("Midi Control: 2 Value: 127")
+
+
+# def test_send_midi_message_no_output():
+#     window_mock = MagicMock()
+#     midi_handler = MidiHandler(window_mock)
+#     midi_handler.midi_output = None
+
+#     with patch.object(QMessageBox, 'warning') as mock_warning:
+#         midi_handler.send_midi_message(pc_number=1, cc_number=None, cc_value=None)
+
+#     assert mock_warning.called_with(
+#         None,
+#         "MIDI output port not found",
+#         "Please connect a valid MIDI output port and try again",
+#     )
+#     assert not midi_handler.midi_output.send.called
+#     assert not window_mock.update_status_bar.called
+
+# def test_get_output():
+#     with patch.object(mido, 'get_output_names', return_value=['output_port1', 'output_port2']):
+#         output_ports = MidiHandler.get_output()
+
+#     assert output_ports == ['output_port1', 'output_port2']
+
+# def test_open_output():
+#     with patch.object(mido, 'open_output', return_value='output_port_mock'):
+#         output_port = MidiHandler.open_output("selected_port")
+
+#     assert output_port == 'output_port_mock'
+#     mido.open_output.assert_called_once_with("selected_port")
---
+++ tests/test_profile_manager.py
@@ -0,0 +1,237 @@
+import os
+import json
+import logging
+from unittest.mock import MagicMock, patch
+import pytest
+from PyQt5.QtWidgets import QFileDialog
+from common.profile_manager import ProfileManager
+
+
+@pytest.fixture
+def mock_qfiledialog():
+    with patch("common.profile_manager.QFileDialog") as mock:
+        yield mock
+
+
+@pytest.fixture
+def mock_qmessagebox():
+    with patch("common.profile_manager.QMessageBox") as mock:
+        yield mock
+
+
+def test_init():
+    script_directory = "/path/to/scripts"
+    version = "1.0"
+    profile_manager = ProfileManager(script_directory, version)
+    assert profile_manager.script_directory == script_directory
+    assert profile_manager.version == version
+    assert profile_manager.window is None
+    assert profile_manager.profile_data is None
+    assert profile_manager.settings == {
+        "port_name": "",
+        "channel": 0,
+        "profile": "default.json",
+        "icon": "icon.icns",
+    }
+
+
+def test_reload_window():
+    script_directory = "/path/to/scripts"
+    version = "1.0"
+    profile_manager = ProfileManager(script_directory, version)
+    window_mock = MagicMock()
+    profile_manager.reload_window(window_mock)
+    assert profile_manager.window == window_mock
+
+
+def test_reload_profile_data():
+    script_directory = "/path/to/scripts"
+    version = "1.0"
+    profile_manager = ProfileManager(script_directory, version)
+    profile_data_mock = {"name": "Test Profile", "channel": 1, "buttons": []}
+    profile_manager.reload_profile_data(profile_data_mock)
+    assert profile_manager.profile_data == profile_data_mock
+
+
+def test_ensure_directories_exist(tmpdir):
+    script_directory = str(tmpdir)
+    version = "1.0"
+    profile_manager = ProfileManager(script_directory, version)
+    profile_manager.ensure_directories_exist()
+    profiles_directory = os.path.join(script_directory, "profiles")
+    assert os.path.isdir(profiles_directory)
+
+
+# def test_load_profile_data(tmpdir):
+#     script_directory = str(tmpdir)
+#     version = "1.0"
+#     profile_manager = ProfileManager(script_directory, version)
+
+#     profile_name = "test_profile.json"
+#     profile_path = os.path.join(script_directory, "profiles", profile_name)
+
+#     # Create a test profile file
+#     test_profile_data = {"name": "Test Profile", "channel": 1, "buttons": []}
+#     with open(profile_path, "w") as profile_file:
+#         json.dump(test_profile_data, profile_file)
+
+#     loaded_profile_data = profile_manager.load_profile_data(profile_name)
+#     assert loaded_profile_data == test_profile_data
+
+
+def test_load_profile_data_nonexistent(tmpdir, caplog):
+    script_directory = str(tmpdir)
+    version = "1.0"
+    profile_manager = ProfileManager(script_directory, version)
+
+    profile_name = "nonexistent_profile.json"
+    with caplog.at_level(logging.WARNING):
+        loaded_profile_data = profile_manager.load_profile_data(profile_name)
+        assert "Profile file not found" in caplog.text
+        assert loaded_profile_data == {
+            "name": "New Profile",
+            "channel": 0,
+            "buttons": [],
+        }
+
+
+# def test_load_profile_data_invalid(tmpdir, caplog):
+#     script_directory = str(tmpdir)
+#     version = "1.0"
+#     profile_manager = ProfileManager(script_directory, version)
+
+#     profile_name = "invalid_profile.json"
+#     profile_path = os.path.join(script_directory, "profiles", profile_name)
+
+#     # Create an invalid JSON file
+#     with open(profile_path, "w") as profile_file:
+#         profile_file.write("invalid_json")
+
+#     with caplog.at_level(logging.ERROR):
+#         with pytest.raises(ProfileNotValid):
+#             profile_manager.load_profile_data(profile_name)
+
+
+def test_load_settings(tmpdir):
+    script_directory = str(tmpdir)
+    version = "1.0"
+    profile_manager = ProfileManager(script_directory, version)
+
+    settings_filename = "settings.json"
+    settings_path = os.path.join(script_directory, settings_filename)
+
+    # Create a test settings file
+    test_settings = {
+        "port_name": "/dev/ttyUSB0",
+        "channel": 1,
+        "profile": "custom.json",
+        "icon": "custom_icon.icns",
+    }
+    with open(settings_path, "w") as settings_file:
+        json.dump(test_settings, settings_file)
+
+    loaded_settings = profile_manager.load_settings(settings_filename)
+    assert loaded_settings == test_settings
+
+
+def test_load_settings_nonexistent(tmpdir, caplog):
+    script_directory = str(tmpdir)
+    version = "1.0"
+    profile_manager = ProfileManager(script_directory, version)
+
+    settings_filename = "nonexistent_settings.json"
+    with caplog.at_level(logging.WARNING):
+        loaded_settings = profile_manager.load_settings(settings_filename)
+        assert "Settings file not found" in caplog.text
+        assert loaded_settings == {
+            "port_name": "",
+            "channel": 0,
+            "profile": "default.json",
+            "icon": "icon.icns",
+        }
+
+
+def test_load_settings_invalid(tmpdir, caplog):
+    script_directory = str(tmpdir)
+    version = "1.0"
+    profile_manager = ProfileManager(script_directory, version)
+
+    settings_filename = "invalid_settings.json"
+    settings_path = os.path.join(script_directory, settings_filename)
+
+    # Create an invalid JSON file
+    with open(settings_path, "w") as settings_file:
+        settings_file.write("invalid_json")
+
+    with caplog.at_level(logging.ERROR):
+        loaded_settings = profile_manager.load_settings(settings_filename)
+        assert "Error loading settings" in caplog.text
+        assert loaded_settings == {
+            "port_name": "",
+            "channel": 0,
+            "profile": "default.json",
+            "icon": "icon.icns",
+        }
+
+
+# def test_save_settings(tmpdir):
+#     script_directory = str(tmpdir)
+#     version = "1.0"
+#     profile_manager = ProfileManager(script_directory, version)
+
+#     # Create a temporary settings file
+#     settings_filename = "settings.json"
+#     settings_path = os.path.join(script_directory, settings_filename)
+#     test_settings = {"port_name": "/dev/ttyUSB0", "channel": 1, "profile": "custom.json", "icon": "custom_icon.icns"}
+#     with open(settings_path, "w") as settings_file:
+#         json.dump(test_settings, settings_file)
+
+#     # Save new settings
+#     new_profile_name = "new_profile.json"
+#     new_channel = 2
+#     new_profile_data = {"name": "New Profile", "channel": new_channel, "buttons": []}
+
+#     with patch("builtins.open", create=True) as mock_open:
+#         profile_manager.save_settings(new_profile_name, new_channel, new_profile_data)
+
+#     assert profile_manager.settings["profile"] == new_profile_name
+#     assert profile_manager.settings["channel"] == new_channel
+
+#     # Ensure that the new settings are saved to the file
+#     mock_open.assert_called_once_with(
+#         os.path.join(script_directory, "settings.json"), "w"
+#     )
+#     written_data = json.dumps(test_settings, indent=4)
+#     mock_open().write.assert_called_once_with(written_data)
+
+# def test_change_profile_dialog_accepted(mock_qfiledialog, tmpdir):
+#     script_directory = str(tmpdir)
+#     version = "1.0"
+#     profile_manager = ProfileManager(script_directory, version)
+#     profile_manager.window = MagicMock()
+
+#     selected_file = os.path.join(script_directory, "profiles", "test_profile.json")
+#     with patch.object(profile_manager, "load_profile_data", return_value={}):
+#         profile_data = profile_manager.change_profile(selected_file)
+
+#     assert profile_data == {}
+#     assert profile_manager.settings["profile"] == "test_profile.json"
+#     assert profile_manager.profile_data == {}
+
+
+def test_change_profile_dialog_canceled(mock_qfiledialog, tmpdir):
+    script_directory = str(tmpdir)
+    version = "1.0"
+    profile_manager = ProfileManager(script_directory, version)
+    profile_manager.window = MagicMock()
+
+    mock_qfiledialog_instance = mock_qfiledialog.return_value
+    mock_qfiledialog_instance.exec_.return_value = QFileDialog.Rejected
+
+    profile_manager.change_profile()
+
+    assert profile_manager.settings["profile"] == "default.json"
+    assert profile_manager.profile_data is None
+    profile_manager.window.update_status_bar.assert_called_once_with(
+        "No profile selected"
+    )
