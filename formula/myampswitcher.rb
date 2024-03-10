60c9c1e2b6f440dd069fffbee7f2112753efc776
Paolo Frigo <paolofrigo@gmail.com>
2024-03-10 12:27:49 +0100

    fix: style for cask def

--- formula/myampswitcher.rb
+++ formula/myampswitcher.rb
@@ -1,23 +1,22 @@
+# typed: true
+# frozen_string_literal: true
+
 cask "myampswitcher" do
-    name "MyAmpSwitcher"
-    description "MyAmpSwitcher is a Python application that allows you to control your amplifier, amp-cab-switcher or any device through MIDI messages. You can switch between different profiles, each containing customizable buttons that send MIDI Program Change and Control Change messages. This software is released under the MIT license."
-    version :latest
-    homepage "https://github.com/PaoloFrigo/my-amp-switcher"
+  version :latest
+  description "MyAmpSwitcher is a Python app that allows you to control any device through MIDI messages."
+  sha256 :no_check
 
-    livecheck do
-      url "https://api.github.com/repos/paolofrigo/my-amp-switcher/releases/latest"
-      regex(%r{"tag_name":\s*"(.*?)".*?"name":\s*"(.*?)".*?"browser_download_url":\s*"(.*?)".*?}m)
-      strategy :page_match do |page, regex|
-        page.scan(regex).map { |match| match[0] }
-      end
-    end
+  url "https://github.com/paolofrigo/my-amp-switcher/releases/latest/download/MyAmpSwitcher.dmg"
+  name "MyAmpSwitcher"
+  homepage "https://github.com/PaoloFrigo/my-amp-switcher"
 
-    url "https://github.com/paolofrigo/my-amp-switcher/releases/latest/download/MyAmpSwitcher.dmg"
-    sha256 :no_check
-
-    app "MyAmpSwitcher.app"
-
-
-    caveats do
+  livecheck do
+    url "https://api.github.com/repos/paolofrigo/my-amp-switcher/releases/latest"
+    regex(/"tag_name":\s*"(.*?)".*?"name":\s*"(.*?)".*?"browser_download_url":\s*"(.*?)".*?/m)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| match[0] }
     end
   end
+
+  app "MyAmpSwitcher.app"
+end
